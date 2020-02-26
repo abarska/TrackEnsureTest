e6ed0d620707cbacb4ba3e1b43e3600582278275
abarska <anastasiya.barska@gmail.com>
2020-02-26 14:30:10 +0200

    update and delete data in firestore when connected

--- app/src/main/java/com/abarska/trackensuretest/viewmodels/ListViewModel.kt
+++ app/src/main/java/com/abarska/trackensuretest/viewmodels/ListViewModel.kt
@@ -1,11 +1,16 @@
 package com.abarska.trackensuretest.viewmodels
 
 import android.app.Application
+import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
+import com.abarska.trackensuretest.R
 import com.abarska.trackensuretest.database.GasStationDatabase
+import com.abarska.trackensuretest.entities.STATION_TABLE
 import com.abarska.trackensuretest.entities.Station
 import com.abarska.trackensuretest.utils.hasInternetConnection
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QueryDocumentSnapshot
 import kotlinx.coroutines.launch
 import java.io.Serializable
 
@@ -14,6 +19,8 @@ class ListViewModel(val app: Application) : AndroidViewModel(app), Serializable 
     private val stationDao = GasStationDatabase.getInstance(app).stationDao
 
     val stations = stationDao.getAllStations()
+
+    private val database = FirebaseFirestore.getInstance()
 
     fun deleteStationInRoom(key: String) {
         viewModelScope.launch { stationDao.delete(key) }
@@ -40,11 +47,30 @@ class ListViewModel(val app: Application) : AndroidViewModel(app), Serializable 
     }
 
     private fun updateNow(station: Station) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val docRef = database
+            .collection(app.applicationContext.getString(R.string.stations))
+            .document(station.id)
+        docRef.set(station).addOnSuccessListener {
+            Toast.makeText(app.applicationContext, R.string.updated, Toast.LENGTH_SHORT).show()
+        }
     }
 
-    private fun deleteNow(id: String){
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    private fun deleteNow(id: String) {
+        val docRef = database
+            .collection(app.applicationContext.getString(R.string.stations))
+            .document(id)
+        val subCollection =
+            docRef.collection(app.applicationContext.getString(R.string.fueling_acts))
+        subCollection.get().addOnCompleteListener { task ->
+            task.let {
+                for (doc in task.result!!) {
+                    subCollection.document(doc.id).delete()
+                }
+            }
+        }
+        docRef.delete().addOnSuccessListener {
+            Toast.makeText(app.applicationContext, R.string.deleted, Toast.LENGTH_SHORT).show()
+        }
     }
 
     private fun launchServiceToUpdate(station: Station) {
